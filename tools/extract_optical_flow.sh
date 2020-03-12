d8d3761d1d7fef8c51e98f0d051b506e29699844
swordli <swordli@tencent.com>
2020-03-12 19:31:49 +0800

    delete

--- tools/extract_optical_flow.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SRC_FOLDER=$1
-OUT_FOLDER=$2
-NUM_WORKER=$3
-
-echo "Extracting optical flow from videos in folder: ${SRC_FOLDER}"
-python ./build_of.py ${SRC_FOLDER} ${OUT_FOLDER} --num_worker ${NUM_WORKER} --new_width 224 --new_height 224 2>local/errors.log
